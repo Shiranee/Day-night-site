517e17b71cf48074245d37c0b60729135bd1b3ad
Vinicius De Oliveira Rampazo <106934638+Shiranee@users.noreply.github.com>
2022-06-16 22:20:48 -0300

    Add files via upload

---
+++ br/script.js
@@ -0,0 +1,6 @@
+function button() {
+    document.body.classList.toggle("dark-theme")
+        
+    var className = document.body.className;
+    console.log('current class name:' + className);
+};
